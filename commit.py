3c69d58d510a7b288f23c1e3c5edb765c66e81a9
Tiago <68362381+suminsky2304@users.noreply.github.com>
2022-01-25 22:36:20 -0300

    'm'

--- commit.py
+++ commit.py
@@ -6,7 +6,7 @@ start_time = time()
 first_time = True
 
 while True:
-	seconds_before_update = 5
+	seconds_before_update = 30
 
 	if (time() - start_time >= seconds_before_update) or first_time:
 		first_time = False
