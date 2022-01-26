258e583259f29719b3a6eed4b2b5b23394a501bc
Tiago <68362381+suminsky2304@users.noreply.github.com>
2022-01-25 22:33:31 -0300

    'm'

---
+++ run_commit.py
@@ -0,0 +1,18 @@
+import os
+from datetime import datetime
+from time import time
+
+start_time = time()
+first_time = True
+
+while True:
+	seconds_before_update = 5
+
+	if (time() - start_time >= seconds_before_update) or first_time:
+		first_time = False
+		start_time = time()
+
+		
+		os.system("git add .")
+		os.system("git commit -m 'm'")
+		os.system("git push")
