9ccf71b4a84987307d2122e6a027fd38ff021387
Tiago <68362381+suminsky2304@users.noreply.github.com>
2022-01-25 23:45:50 -0300

    'm'

--- run US.py
+++ run US.py
@@ -5,20 +5,20 @@ start_time = time()
 first_time = True
 
 while True:
-	seconds_before_update = 500
+	seconds_before_update = 6000
 
 	if (time() - start_time >= seconds_before_update) or first_time:
 		first_time = False
 		start_time = time()
 
-		print(datetime.now(), 'IN')
+		print(datetime.now(), 'IN_US')
 		with open('contas.txt', 'r') as f:
 			lines = [_.strip() for _ in f.readlines()]
 
-		print(datetime.now(), 'OUT')
+		print(datetime.now(), 'OUT_US')
 		with open('reset_contas.txt', 'w') as f:
-			f.write('\n'.join(lines[:10]))
+			f.write('\n'.join(lines[:100]))
 
 		with open('contas.txt', 'w') as f:
-			f.write('\n'.join(lines[10:]))
+			f.write('\n'.join(lines[100:]))
 
