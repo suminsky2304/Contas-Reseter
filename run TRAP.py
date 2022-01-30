4d3d5bcc0b3108988127d7f6a2a876c712d63133
Tiago <68362381+suminsky2304@users.noreply.github.com>
2022-01-30 13:36:35 -0300

    'm'

--- run TRAP.py
+++ run TRAP.py
@@ -5,7 +5,7 @@ start_time = time()
 first_time = True
 
 while True:
-	seconds_before_update = 77
+	seconds_before_update = 203
 
 	if (time() - start_time >= seconds_before_update) or first_time:
 		first_time = False
