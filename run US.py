eddcaf04311d73a40b3e52a1092c8e9ceaf152f6
Tiago <68362381+suminsky2304@users.noreply.github.com>
2022-02-17 11:31:17 -0300

    'm'

--- run US.py
+++ run US.py
@@ -5,7 +5,7 @@ start_time = time()
 first_time = True
 
 while True:
-	seconds_before_update = 600
+	seconds_before_update = 80
 
 	if (time() - start_time >= seconds_before_update) or first_time:
 		first_time = False
